693a4267c14a1b9ebd32556c537448c1b91239ed
JarminJS <jarminjj2@gmail.com>
2022-12-11 13:52:47 +0800

    First Push

---
+++ resources/views/index.blade.php
@@ -0,0 +1,365 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900" rel="stylesheet">
+
+    <title>FYP Management System</title>
+<!--
+SOFTY PINKO
+https://templatemo.com/tm-535-softy-pinko
+-->
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+
+    <link rel="stylesheet" href="assets/css/templatemo-softy-pinko.css">
+
+    </head>
+    
+    <body>
+    
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>  
+    <!-- ***** Preloader End ***** -->
+    
+    
+    <!-- ***** Header Area Start ***** -->
+    <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+                        <!-- ***** Logo Start ***** -->
+                        <a href="#" class="logo">
+                            <img src="assets/images/logo.png" alt="Softy Pinko"/>
+                        </a>
+                        <!-- ***** Logo End ***** -->
+                        <!-- ***** Menu Start ***** -->
+                        <ul class="nav">
+                            <li><a href="#welcome" class="active">Home</a></li>
+                            <li><a href="#deliverables">About</a></li>
+                            <li><a href="#countdown">Countdown</a></li>
+                            {{-- <li><a href="#testimonials">Testimonials</a></li>
+                            <li><a href="#pricing-plans">Pricing Tables</a></li>
+                            <li><a href="#blog">Blog Entries</a></li>
+                            <li><a href="#contact-us">Contact Us</a></li> --}}
+                        </ul>
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+                        <!-- ***** Menu End ***** -->
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
+
+    <!-- ***** Welcome Area Start ***** -->
+    <div class="welcome-area" id="welcome">
+
+        <!-- ***** Header Text Start ***** -->
+        <div class="header-text">
+            <div class="container">
+                <div class="row">
+                    <div class="offset-xl-3 col-xl-6 offset-lg-2 col-lg-8 col-md-12 col-sm-12">
+                        <h1><strong>Final Year Project (FYP)</strong></h1>
+                        <p>Final Year Project (FYP) is one of the requirements that CCI students need to take in order to graduate and it is divided into two phases Project 1 (CGNB413) and Project 2 (CGNB424).</p>
+
+                        <a href="#deliverables" class="main-button-slider">Project 1 Deliverables</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- ***** Header Text End ***** -->
+    </div>
+    <!-- ***** Welcome Area End ***** -->
+
+    <!-- ***** Features Small Start ***** -->
+    <section class="section home-feature">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row">
+                        <!-- ***** Features Small Item Start ***** -->
+                        <div class="col-lg-4 col-md-6 col-sm-6 col-12" data-scroll-reveal="enter bottom move 50px over 0.6s after 0.2s">
+                            <div class="features-small-item">
+                                <div class="icon">
+                                    <i><img src="assets/images/featured-item-03.png" alt=""></i>
+                                </div>
+                                <h5 class="features-title">Apply Knowledge</h5>
+                                <p>Apply skills and knowledge acquired from previous classes</p>
+                            </div>
+                        </div>
+                        <!-- ***** Features Small Item End ***** -->
+
+                        <!-- ***** Features Small Item Start ***** -->
+                        <div class="col-lg-4 col-md-6 col-sm-6 col-12" data-scroll-reveal="enter bottom move 50px over 0.6s after 0.4s">
+                            <div class="features-small-item">
+                                <div class="icon">
+                                    <i><img src="assets/images/featured-item-01.png" alt=""></i>
+                                </div>
+                                <h5 class="features-title">Gain Experience</h5>
+                                <p>Gain more experience in developing system from the start to finish</p>
+                            </div>
+                        </div>
+                        <!-- ***** Features Small Item End ***** -->
+
+                        <!-- ***** Features Small Item Start ***** -->
+                        <div class="col-lg-4 col-md-6 col-sm-6 col-12" data-scroll-reveal="enter bottom move 50px over 0.6s after 0.6s">
+                            <div class="features-small-item">
+                                <div class="icon">
+                                    <i><img src="assets/images/featured-item-02.png" alt=""></i>
+                                </div>
+                                <h5 class="features-title">Learn New Technology</h5>
+                                <p>Integrate new technologies and technique not taught in class</p>
+                            </div>
+                        </div>
+                        <!-- ***** Features Small Item End ***** -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Features Small End ***** -->
+    
+    <!-- ***** Features Big Item Start ***** -->
+    <section class="section padding-top-70 padding-bottom-0" id="deliverables">
+        <div class="container">
+
+            
+                <div class="row margin-bottom-30">
+                    <h1 class="mx-auto"><strong>Project 1 Deliverables </strong></h3>
+                </div>
+            
+            <div class="row">
+                <div class="col-lg-5 col-md-12 col-sm-12 align-self-center" data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
+                    <img src="assets/images/left-image.png" class="rounded img-fluid d-block mx-auto" alt="App">
+                </div>
+                <div class="col-lg-1"></div>
+                <div class="col-lg-6 col-md-12 col-sm-12 align-self-center mobile-top-fix">
+                    <div class="left-heading">
+                        <h2 class="section-title">Milestone 1 Requirements</h2>
+                    </div>
+                    <div class="left-text">
+                        <p>
+                            <strong>Chapter 1 (Introduction)</strong>
+                            <ul>
+                                <li>Background</li>
+                                <li>Problem Statements</li>
+                                <li>Project Objectives</li>
+                            </ul>
+                        </p>
+                        <p>
+                            <strong>Chapter 2 (Related Works)</strong>
+                            <ul>
+                                <li>Review on Existing System</li>
+                                <li>Comparison Table</li>
+                                
+                            </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="hr"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <section class="section padding-bottom-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-12 col-sm-12 align-self-center mobile-bottom-fix">
+                    <div class="left-heading">
+                        <h2 class="section-title">Milestone 2 Requirements</h2>
+                    </div>
+                    <div class="left-text">
+                        <p>
+                            <strong>Chapter 3 (Requirement Analysis)</strong>
+                            <ul>
+                                <li>Requirement Elicitation: </li>
+                                <li>i. Elicitation Technique</li>
+                                <li>i. Results and Discussion</li>
+                                
+                                <br>
+                                <li>Requirement Specification </li>
+                                
+                                <br>
+                                <li>Appendix: SRS</li>
+                            </ul>
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-1"></div>
+                <div class="col-lg-5 col-md-12 col-sm-12 align-self-center mobile-bottom-fix-big" data-scroll-reveal="enter right move 30px over 0.6s after 0.4s">
+                    <img src="assets/images/right-image.png" class="rounded img-fluid d-block mx-auto" alt="App">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section padding-top-70 padding-bottom-0" id="features">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-md-12 col-sm-12 align-self-center" data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
+                    <img src="assets/images/left-image1.png" class="rounded img-fluid d-block mx-auto" alt="App">
+                </div>
+                <div class="col-lg-1"></div>
+                <div class="col-lg-6 col-md-12 col-sm-12 align-self-center mobile-top-fix">
+                    <div class="left-heading">
+                        <h2 class="section-title">Milestone 3(a) Requirements</h2>
+                    </div>
+                    <div class="left-text">
+                        <p>
+                            <strong>Chapter 4 (Design)</strong>
+                            <ul>
+                                <li>Architecture Diagram</li>
+                                <li>Interface Diagram</li>
+                                <li>Database Design</li>
+                            </ul>
+                        </p>
+                        <p>
+                            <strong>Chapter 5 (Conclusion)</strong>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="hr"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section padding-bottom-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-12 col-sm-12 align-self-center mobile-bottom-fix">
+                    <div class="left-heading">
+                        <h2 class="section-title">Milestone 3(b) Requirements</h2>
+                    </div>
+                    <div class="left-text">
+                        <p>
+                            <strong>Complete Final Report: </strong>
+                            <p>Chapter 1 - Chapter 5</p>
+                        </p>
+                        
+                    </div>
+                </div>
+                <div class="col-lg-1"></div>
+                <div class="col-lg-5 col-md-12 col-sm-12 align-self-center mobile-bottom-fix-big" data-scroll-reveal="enter right move 30px over 0.6s after 0.4s">
+                    <img src="assets/images/right-image1.png" class="rounded img-fluid d-block mx-auto" alt="App">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Features Big Item End ***** -->
+
+    <section class="counter" id="countdown">
+        <div class="content">
+            <div class="container">
+                <div class="row padding-top-70">
+                    <h1 class="mx-auto text-white"><strong>FYP 1 Presentation: 31st Jan 2023, 8.00 AM </strong></h1>
+
+                </div>
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 col-sm-12" >
+                        <div class="count-item decoration-bottom">
+                            <strong id="days"></strong>
+                            <span>Days</span>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-12">
+                        <div class="count-item decoration-top">
+                            <strong id="hours"></strong>
+                            <span>Hours</span>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-12">
+                        <div class="count-item decoration-bottom">
+                            <strong id="minutes"></strong>
+                            <span>Minutes</span>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-12">
+                        <div class="count-item">
+                            <strong id="seconds"></strong>
+                            <span>Seconds</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script>
+        var presentation = new Date("Jan 31, 2023 08:00:00").getTime(); 
+
+        var x = setInterval(() => {
+        
+        var now = new Date().getTime(); 
+        var t = presentation - now; 
+
+        var days = Math.floor(t / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((t % (1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+        var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((t % (1000 * 60)) / 1000);
+
+        document.getElementById("days").innerHTML = days; 
+        document.getElementById("hours").innerHTML = hours; 
+        document.getElementById("minutes").innerHTML = minutes; 
+        document.getElementById("seconds").innerHTML = seconds; 
+        }, 1000);
+        
+
+    </script>
+        
+     
+    <!-- ***** Footer Start ***** -->
+    <footer>
+        <div class="container">
+            
+            <div class="row">
+                <div class="col-lg-12">
+                    <p class="copyright">Copyright &copy; 2020 Jarmin Anak Jack SW0107517</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    
+    <!-- jQuery -->
+    <script src="assets/js/jquery-2.1.0.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="assets/js/popper.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- Plugins -->
+    <script src="assets/js/scrollreveal.min.js"></script>
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <script src="assets/js/imgfix.min.js"></script> 
+    
+    <!-- Global Init -->
+    <script src="assets/js/custom.js"></script>
+
+  </body>
+</html>
